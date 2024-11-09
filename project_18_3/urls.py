8d20708f70dc58d089df4030cb3f857855f39a15
srivarshithagatta <srivarshithagatta@gmail.com>
2024-11-09 12:44:41 +0530

    fifth_commit

--- project_18_3/urls.py
+++ project_18_3/urls.py
@@ -19,9 +19,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app_18_3.urls')),  # Includes the URLs from app_18_3 at the root URL
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
